0a7ad30a37a5406a30d0f3773286f35d8c9e5ab8
Ankit Kumar <100723495+1820ANKIT2029@users.noreply.github.com>
2025-12-08 10:44:13 +0000

    Add modular inverse function and implement prime number utilities

--- maths.cpp
+++ maths.cpp
@@ -58,6 +58,9 @@ long long nCr(int n, int r){
     return (nume * power(deno, MOD-2)) % MOD;
 }
 
+long long modInverse(long long n) {
+    return power(n, MOD - 2);
+}
 
 /*
     Extended Euclidean Algorithm
@@ -75,4 +78,75 @@ int extEuclid(int a, int b, int &x, int &y) {      // pass x and y by ref
         t = yy; yy = y-q*yy; y = t;                // t
     }
     return a;                                      // returns gcd(a, b)
+}
+
+
+/*
+    Prime Number
+
+    1. bool isPrime(long long n):  
+        O(n) : 2..(n-1)
+        O(n^0.5) : 2..(n^0.5)
+        O(n^0.5 / 2) : 2, 3, 5, 7 .. (n^0.5), only odd number except 2
+        O(n^0.5 / ln(n^0.5)):  if list of prime number < n^0.5 known, O(m/ln(m)) number of primes less than m.
+        
+    2. sieve(n): generate list of prime numbers < n
+        O(nlnln(n))
+
+        to cheak if a number is prime: O(nlnln(n) + (n^0.5 / ln(n^0.5)))
+*/
+
+bool isPrime(long long n) {
+    long long n_sqrt = sqrt(n);
+    if(n == 2) return true;
+    if(n < 2 || n % 2 == 0) return false;
+    
+    for(long long i=3; i<=n_sqrt; i+=2) {
+        if(n % i == 0) return false;
+    }
+
+    return true;
+}
+
+// global variable for sieve
+long long sieve_size;
+bitset<10000010> bs;                     // 10^7 limit
+vector<long long> prime;
+
+void sieve(long long upperbound) {
+    sieve_size = upperbound + 1;
+    bs.set();
+    bs[0] = bs[1] = 0;
+
+    for(int i=2; i<sieve_size; i++) {
+        if(bs[i]) {
+            for(int j=i*i; j<sieve_size; j+=i) bs[j] = 0;
+            prime.push_back(i);
+        }
+    }
+}
+
+// uses sieve
+bool isPrime(long long n) {
+    if(n < sieve_size) return bs[n];
+
+    int m = prime.size();
+    for(int i=0; i<m && (prime[i]*prime[i] <= n); i++)
+        if(n % prime[i] == 0)
+            return false;
+    return true;
+}
+
+vector<long long> primeFactor(long long n) {
+    vector<long long> factors;
+    int m = prime.size();
+    for(int i=0; i<m && (prime[i]*prime[i] <= n); i++) {
+        while(n % prime[i] == 0) {
+            N /= prime[i];
+            factors.push_back(prime[i]);
+        }
+    }
+
+    if(n != 1) factors.push_back(n);
+    return factors;
 }
