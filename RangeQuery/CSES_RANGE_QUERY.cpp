6775fbab2d269ba5138d04bcfba0ae8ad303c7fc
Ankit Kumar <100723495+1820ANKIT2029@users.noreply.github.com>
2026-02-09 20:28:59 +0530

    Update CSES_RANGE_QUERY.cpp

--- RangeQuery/CSES_RANGE_QUERY.cpp
+++ RangeQuery/CSES_RANGE_QUERY.cpp
@@ -427,6 +427,371 @@ class _1652 { // 2D prefix sum
 	}
 };
 
+class segmentTreeFirstElementGreaterThanK { // First Element >= k
+	int size;
+	vector<ll> tree;
+	
+	void build(int p, int L, int R, vector<ll> &a) {
+		if(L == R) {
+			if(L < (int)a.size()) tree[p] = a[L];
+			return;
+		}
+		
+		int mid = L + ((R - L) >> 1);
+		build(Left(p), L, mid, a);
+		build(Right(p), mid+1, R, a);
+		
+		tree[p] = max(tree[Left(p)], tree[Right(p)]);
+	}
+	
+	void update(int p, int L, int R, int ind, int v) {
+		if(L == R) {
+			tree[p] -= v; return;
+		}
+		
+		int mid = L + ((R-L)>>1);
+		if(ind <= mid) update(Left(p), L, mid, ind, v);
+		else update(Right(p), mid+1, R, ind, v);
+		
+		tree[p] = max(tree[Left(p)], tree[Right(p)]);
+	}
+	
+	int find(int p, int L, int R, ll v) {
+		if(tree[p] < v) return -1;
+		if(L == R) {
+			if(v <= tree[p]) return L;
+			return -1;
+		}
+		
+		int ans, mid = L + ((R - L) >> 1);
+		ans = find(Left(p), L, mid, v);
+		if(ans != -1) return ans;
+		ans = find(Right(p), mid+1, R, v);
+		return ans;
+	}
+	
+public:
+	segmentTreeFirstElementGreaterThanK(vector<ll> &a) {
+		int n = a.size();
+		size = 1;
+		while(size < n) size *= 2;
+		
+		tree.assign(2 * size, -1);
+		
+		build(0, 0, size - 1, a);
+	}
+	
+	void update(int ind, int v) {
+		update(0, 0, size-1, ind, v);
+	}
+	
+	int find(ll v) {
+		int ans = find(0, 0, size-1, v);
+		if(ans != -1) update(ans, v);
+		return ans;
+	}
+};
+
+class _1143 {
+	public:
+	int n, m;
+	vector<ll> h;
+	void solve() {
+		FAST_IO;
+		cin >> n >> m;
+		h.resize(n);
+		for(ll &i: h) cin >> i;
+		
+		segmentTreeFirstElementGreaterThanK seg(h);
+		
+		ll r;
+		while(m--) {
+			cin >> r;
+			
+			cout << seg.find(r) + 1 << " ";
+		}
+		cout << endl;
+	}
+};
+
+class segmentTreeListRemovals {
+	int size;
+	vector<int> tree, nums;
+	
+	void build(int p, int L, int R, vector<int> &a) {
+		if(L == R) {
+			if(L < (int)a.size()) tree[p] = 1;
+			return;
+		}
+		
+		int mid = L + ((R-L) >> 1);
+		build(Left(p), L, mid, a);
+		build(Right(p), mid+1, R, a);
+		
+		tree[p] = tree[Left(p)] + tree[Right(p)];
+	}
+	
+	void update(int p, int L, int R, int ind, int v) {
+		if(L == R) { tree[p] -= v; return; }
+		
+		int mid = L + ((R-L)>>1);
+		if(ind <= mid) update(Left(p), L, mid, ind, v);
+		else update(Right(p), mid+1, R, ind, v);
+		
+		tree[p] = tree[Left(p)] + tree[Right(p)];
+	}
+	
+	int find(int p, int L, int R, int ind) {
+		if(L == R) return L;
+		
+		int mid = L + ((R-L)>>1);
+		if(ind < tree[Left(p)]) return find(Left(p), L, mid, ind);
+		return find(Right(p), mid+1, R, ind - tree[Left(p)]);
+	}
+	
+public:
+	segmentTreeListRemovals(vector<int> &a): nums(a) {
+		int n = a.size();
+		size = 1;
+		while(size < n) size *= 2;
+		tree.assign(2 * size, 0);
+		
+		build(0, 0, size - 1, a);
+	}
+	
+	void update(int ind, int v) {
+		update(0, 0, size-1, ind, v);
+	}
+	
+	int find(int ind) {
+		int i = find(0, 0, size-1, ind);
+		update(i, 1);
+		return nums[i];
+	}
+};
+
+class _1749 {
+	public:
+	int n;
+	vector<int> x;
+	
+	void solve() {
+		FAST_IO;
+		cin >> n;
+		x.resize(n);
+		for(int &i: x) cin >> i;
+		
+		segmentTreeListRemovals seg(x);
+		
+		int p;
+		while(n--) {
+			cin >> p;
+			cout << seg.find(p - 1) << " ";
+		}
+		cout << endl;
+	}
+};
+
+class segmentTreeCountBtwAAndB {
+	int size;
+	vector<int> tree;
+	
+	void update(int p, int L, int R, int ind, int v) {
+		if(L == R) { tree[p] += v; return; }
+		
+		int mid = L + ((R-L)>>1);
+		if(ind <= mid) update(Left(p), L, mid, ind, v);
+		else update(Right(p), mid+1, R, ind, v);
+		
+		tree[p] = tree[Left(p)] + tree[Right(p)];
+	}
+	
+	int query(int p, int L, int R, int l, int r) {
+		if(l > R || r < L) return 0;
+		if(l <= L && r >= R) return tree[p];
+		
+		int mid = L + ((R-L) >> 1);
+		return query(Left(p), L, mid, l, r) + query(Right(p), mid+1, R, l, r);
+	}
+	
+public:
+	segmentTreeCountBtwAAndB(int n) {
+		size = 1;
+		while(size < n) size *= 2;
+		tree.assign(2*size, 0);
+	}
+	
+	void update(int ind, int v) {
+		update(0, 0, size-1, ind, v);
+	}
+	
+	int query(int l, int r) {
+		if(l > r) return 0;
+		return query(0, 0, size-1, l, r);
+	}
+};
+
+class _1144 {
+	public:
+	int n, q;
+	vector<int> p;
+	
+	void solve() {
+		FAST_IO;
+		cin >> n >> q;
+		p.resize(n);
+		for(int &i: p) cin >> i;
+		
+		vector<pair<char, pair<int, int>>> queries;
+		vector<int> all_values = p;
+		char type; int a, b;
+		for (int i = 0; i < q; i++) {
+			cin >> type >> a >> b;
+			queries.push_back({type, {a, b}});
+			if (type == '!') {
+				all_values.push_back(b);
+			} else {
+				all_values.push_back(a);
+				all_values.push_back(b);
+			}
+		}
+		
+		// Coordinate Compression
+		sort(all_values.begin(), all_values.end());
+		all_values.erase(unique(all_values.begin(), all_values.end()), all_values.end());
+		auto get_id = [&](int x) {
+			return lower_bound(all_values.begin(), all_values.end(), x) - all_values.begin();
+		};
+		
+		int m = all_values.size();
+		segmentTreeCountBtwAAndB st(m);
+		for (int x : p) {
+			st.update(get_id(x), 1);
+		}
+		
+		for (auto &qr : queries) {
+			type = qr.first;
+			a = qr.second.first;
+			b = qr.second.second;
+
+			if (type == '!') {
+				int idx = a - 1;
+				st.update(get_id(p[idx]), -1);
+				p[idx] = b;
+				st.update(get_id(p[idx]), 1);
+			} else {
+				int l = lower_bound(all_values.begin(), all_values.end(), a) - all_values.begin();
+				int r = upper_bound(all_values.begin(), all_values.end(), b) - all_values.begin() - 1;
+				if (l <= r)
+					cout << st.query(l, r) << "\n";
+				else
+					cout << 0 << "\n";
+			}
+		}
+	}
+};
+
+class segmentTreeMaxPrefixSum {
+public:
+	struct item {
+		ll sum, prefixSum;
+	};
+	
+private:
+	int size;
+	vector<item> tree;
+	
+	item NEUTRAL_ELEMENT = {0, 0};
+	
+	item merge(item &a, item &b) {
+		return {
+			a.sum + b.sum,
+			max(a.prefixSum, a.sum + b.prefixSum)
+		};
+	}
+	
+	void build(int p, int L, int R, vector<ll> &a) {
+		if(L == R) {
+			if(L < a.size()) tree[p] = {a[L], max(0LL, a[L])};
+			return;
+		}
+		
+		int mid = L + ((R-L)>>1);
+		build(Left(p), L, mid, a);
+		build(Right(p), mid+1, R, a);
+		
+		tree[p] = merge(tree[Left(p)], tree[Right(p)]);
+	}
+	
+	void update(int p, int L, int R, int idx, int val) {
+		if(L == R) { tree[p] = {val, max(0, val)}; return;}
+		
+		int mid = L + ((R-L)>>1);
+		if(idx <= mid) update(Left(p), L, mid, idx, val);
+		else update(Right(p), mid+1, R, idx, val);
+		
+		tree[p] = merge(tree[Left(p)], tree[Right(p)]);
+	}
+	
+	item rangeQuery(int p, int L, int R, int l, int r) {
+		if(l <= L && r >= R) return tree[p];
+		
+		int mid = L + ((R-L) >> 1);
+		
+		if(r <= mid) return rangeQuery(Left(p), L, mid, l, r);
+		if(l > mid) return rangeQuery(Right(p), mid+1, R, l, r);
+		
+		item ans1 = rangeQuery(Left(p), L, mid, l, r);
+		item ans2 = rangeQuery(Right(p), mid+1, R, l, r);
+		return merge(ans1, ans2);
+	}
+	
+public:
+	segmentTreeMaxPrefixSum(vector<ll> &a) {
+		int n = a.size(); size = 1;
+		while(size < n) size *= 2;
+		
+		tree.assign(2 * size, NEUTRAL_ELEMENT);
+		build(0, 0, size-1, a);
+	}
+	
+	void update(int idx, ll val) {
+		update(0, 0, size-1, idx, val);
+	}
+	
+	ll rangeQuery(int l, int r) {
+		return rangeQuery(0, 0, size-1, l, r).prefixSum;
+	}
+};
+
+class _2166 {
+	public:
+	int n, q;
+	vector<ll> x;
+	
+	void solve() {
+		FAST_IO;
+		cin >> n >> q;
+		x.resize(n);
+		for(ll &i: x) cin >> i;
+		
+		segmentTreeMaxPrefixSum seg(x);
+		
+		int type, k, a, b; ll u;
+		while(q--) {
+			cin >> type;
+			if(type == 1) {
+				cin >> k >> u;
+				seg.update(k-1, u);
+			}
+			else {
+				cin >> a >> b;
+				cout << seg.rangeQuery(a-1, b-1) << endl;
+			}
+		}
+	}
+};
+
 /* templete
 class _QuesNum {
 	public:
@@ -440,7 +805,7 @@ void solve() {
 	
 	//auto start = chrono::high_resolution_clock::now();
 	
-	_1652 a;
+	_2166 a;
 	a.solve();
 	
 	/*
